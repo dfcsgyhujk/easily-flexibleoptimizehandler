2cb60ff44cd7b66c027ac21bb87cc03e7204991b
dfcsgyhujk <sakuraharpsg@hotmail.com>
2024-05-10 05:06:26 +0700

    demo

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-function countAndSay(n) {
-  let result = "1";
-  for (let i = 1; i < n; i++) {
-    let temp = "";
-    let count = 1;
-    for (let j = 0; j < result.length; j++) {
-      if (result[j] === result[j + 1]) {
-        count++;
-      } else {
-        temp += count + result[j];
-        count = 1;
-      }
-    }
-    result = temp;
-  }
-  return result;
-}
+const flattenDepth = (arr, depth = 1) =>
+  arr.reduce(
+    (a, v) =>
+      a.concat(depth > 1 && Array.isArray(v) ? flattenDepth(v, depth - 1) : v),
+    [],
+  );

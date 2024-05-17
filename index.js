a1b74406769b8f982adbdebaaec5a72ccde159ff
dfcsgyhujk <sakuraharpsg@hotmail.com>
2024-05-17 19:11:20 +0700

    init project

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function countWords(str) {
-  return str.split(" ").filter((word) => word !== "").length;
+function maxProfit(prices) {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+  for (const price of prices) {
+    minPrice = Math.min(minPrice, price);
+    maxProfit = Math.max(maxProfit, price - minPrice);
+  }
+  return maxProfit;
 }
